7ee89ac790ef64dd3609b26a62cf8196a1bb61cd
Abdulwahed Mansour <120674623+abd-mansour@users.noreply.github.com>
2023-03-12 05:40:09 +0200

    fix loading ...

--- components/EarthquakeDetails.js
+++ components/EarthquakeDetails.js
@@ -16,9 +16,11 @@ const EarthquakeDetails = () => {
 
     if (earthquakes.length === 0) {
         return (
+            <div className="container">
             <div className="d-flex align-items-center justify-content-center">
-                <strong>Loading...</strong>
+                <strong>تحميل البيانات ...</strong>
                 <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+            </div>
             </div>
         );
     }
